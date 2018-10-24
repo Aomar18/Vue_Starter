2b2ed290bacf5b2fd65837296e19f295b0ce4a74
Ahmed <omar0121@umn.edu>
2018-10-24 16:23:51 -0500

    created function and variables that track the location of the mouse using offset property and are tracked using v-on mouse movements

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ new Vue({
         Name: 'Ahmed',
         age: 26,
         github: "https://github.com/Aomar18",
-        webTag: '<a href="https://github.com/Aomar18">Ahmeds Github</a>'
+        webTag: '<a href="https://github.com/Aomar18">Ahmeds Github</a>',
+        x: 0,
+        y: 0
     },
     methods: {
         add:function(inc){
@@ -16,7 +18,11 @@ new Vue({
         },
         greet:function(time){
             return 'Good ' + time
-         }
-         
+         },
+        updateXY:function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+
+        }
     }
 })
