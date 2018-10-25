327314708b8e08af288a37f85e495b9a4d81b6cf
Ahmed <omar0121@umn.edu>
2018-10-25 10:56:28 -0500

    created computed properties to add numbers

--- app.js
+++ app.js
@@ -8,34 +8,47 @@ new Vue({
         webTag: '<a href="https://github.com/Aomar18/Vue_Starter">My Vue Repo</a>',
         x: 0,
         y: 0,
+        number: 20,
+        a: 0,
+        b: 0,
         dog: '',
-        cat:''
+        cat: ''
     },
     methods: {
-        add:function(inc){
-            this.age+=inc;
+        add: function (inc) {
+            this.age += inc;
         },
-        subtract:function(dec){
-            this.age-=dec;
+        subtract: function (dec) {
+            this.age -= dec;
         },
-        greet:function(time){
+        greet: function (time) {
             return 'Good ' + time
-         },
-        updateXY:function(event){
+        },
+        updateXY: function (event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
         },
-        click:function(){
+        click: function () {
             alert("Sorry You Can't Go There!")
         },
-        logDog: function(){
+        logDog: function () {
             console.log('You entered a dog');
-            
+
         },
-        logCat: function(){
+        logCat: function () {
             console.log('You entered a cat');
 
         }
+    },
+    computed: {
+        addToA: function () {
+            return this.a + this.number
+        },
+        addToB: function () {
+            return this.b + this.number
+        }
 
     }
+
+
 })
